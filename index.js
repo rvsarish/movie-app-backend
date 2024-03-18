d70e37609523ecbb6eef8b5512de29dee8e05b52
R.V.SARISH <21it210@stjosephs.ac.in>
2024-03-18 19:10:07 +0530

    final-class-full-code

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ import bcrypt from "bcrypt";
 import  jwt  from "jsonwebtoken";
 
 const app=express();
-const url="mongodb+srv://sanjai:sanjai123@movies.vcowzfe.mongodb.net/?retryWrites=true&w=majority&appName=movies"
+const url="mongodb+srv://sari:sari@movie.9orvgxr.mongodb.net/?retryWrites=true&w=majority&appName=movie"
 const client= new MongoClient(url);
 await client.connect();
 console.log("mongodb connected");
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
+
 
 const auth =(request,response,next)=>{
     try {
@@ -71,30 +74,30 @@ app.post("/register",async function(request,response){
         const salt = await bcrypt.genSalt(10);
         const hashedPassword=await bcrypt.hash(password,salt);
         const registerMethod=await client.db("CRUD").collection("users").insertOne({username:username,email:email,password:hashedPassword});
-        console.log(hashedPassword);
-        
+        response.status(201).send(registerMethod);
     }
 })
 
 app.post("/login",async function(request,response){
-    const {email,password}=request.body;
+    const {email, password}=request.body;
     const userFind = await client.db("CRUD").collection("users").findOne({email:email});
     if(userFind){
         const mongoDBpassword = userFind.password;
         const passwordCheck = await bcrypt.compare(password,mongoDBpassword);
         if(passwordCheck){
-            const token = jwt.sign({id:userFind._id},"student"); //jwt token 
+            const token = jwt.sign({id:userFind._id},"student",); //jwt token 
             response.status(200).send({token:token});
         }
         else{
-            response.status(400).send("Invalid password")
+            response.status(400).send({message:"Invalid password"})
         }
     }
     else{
-        response.status(400).send("Invalid email");
+        response.status(400).send({message:"Invalid email"});
     }
 })
 
 app.listen(8000,()=>{
     console.log("Server connected successfully..😎");
 })
+
